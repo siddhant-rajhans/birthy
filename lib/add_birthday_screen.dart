3c6f9b202944a75a3e4b7a8e9eeaee24d0b33124
siddhant-rajhans <siddhantrajhans@gmail.com>
2024-07-21 15:37:30 +0530

    Added unique ID generation and editing functionality to the add birthday screen.

--- lib/add_birthday_screen.dart
+++ lib/add_birthday_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
 
 import 'birthday_model.dart';
 
@@ -7,19 +8,31 @@ class AddBirthdayScreen extends StatefulWidget {
   final Function(Birthday) onBirthdayAdded;
   final Birthday? initialBirthday; // Can be null for new birthdays
 
-  const AddBirthdayScreen({Key? key, required this.onBirthdayAdded, this.initialBirthday}) : super(key: key);
+  const AddBirthdayScreen(
+      {Key? key, required this.onBirthdayAdded, this.initialBirthday})
+      : super(key: key);
 
   @override
   State<AddBirthdayScreen> createState() => _AddBirthdayScreenState();
 }
-
 
 class _AddBirthdayScreenState extends State<AddBirthdayScreen> {
   final _nameController = TextEditingController();
   DateTime? _selectedDate;
   int? _id; // Initialize id to null
 
+  @override
+  void initState() {
+    super.initState();
+    if (widget.initialBirthday != null) {
+      _nameController.text = widget.initialBirthday!.name;
+      _selectedDate = widget.initialBirthday!.dateOfBirth;
+      _id = widget.initialBirthday!.id;
+    }
+  }
+
   int generateUniqueId() {
+    var uuid = const Uuid();
     return uuid.v4().hashCode;
   }
 
@@ -40,23 +53,25 @@ class _AddBirthdayScreenState extends State<AddBirthdayScreen> {
             const SizedBox(height: 16.0),
             TextButton(
               onPressed: () => _selectDate(context),
-              child: Text(_selectedDate?.toString().split(' ').first ?? 'Select Date'),
+              child: Text(
+                  _selectedDate?.toString().split(' ').first ?? 'Select Date'),
             ),
             const SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () {
-                if (_nameController.text.isNotEmpty && _selectedDate != null) {
-                  _id ??= generateUniqueId(); // Generate ID if not already set
+                if (_nameController.text.isNotEmpty &&
+                    _selectedDate != null) {
                   final birthday = Birthday(
                     name: _nameController.text,
-                    dateOfBirth: DateTime(_selectedDate!.year, _selectedDate!.month, _selectedDate!.day),
-                    id: _id!,
+                    dateOfBirth: DateTime(_selectedDate!.year,
+                        _selectedDate!.month, _selectedDate!.day),
+                    id: _id ?? generateUniqueId(),
                   );
                   widget.onBirthdayAdded(birthday);
-                  Navigator.pop(context); // Access context from Stateful Widget
+                  Navigator.pop(context);
                 }
               },
-              child: const Text('Add Birthday'),
+              child: const Text('Save Birthday'),
             ),
           ],
         ),
@@ -65,15 +80,16 @@ class _AddBirthdayScreenState extends State<AddBirthdayScreen> {
   }
 
   Future<void> _selectDate(BuildContext context) async {
-    final selectedDate = await showDatePicker(
+    final DateTime? picked = await showDatePicker(
       context: context,
-      initialDate: DateTime.now(),
+      initialDate: _selectedDate ?? DateTime.now(),
       firstDate: DateTime(1900),
-      lastDate: DateTime.now(),
+      lastDate: DateTime(2100),
     );
-    if (selectedDate != null) {
+
+    if (picked != null && picked != _selectedDate) {
       setState(() {
-        _selectedDate = selectedDate;
+        _selectedDate = picked;
       });
     }
   }
